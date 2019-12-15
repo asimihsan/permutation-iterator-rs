053fa59f34c31e20c0d1336add765d3108a3663c
Asim Ihsan <asim.ihsan@gmail.com>
2019-12-15 11:55:48 -0800

    examples - add new deterministic example

---
+++ examples/deterministic.rs
@@ -0,0 +1,10 @@
+use permutation_iterator::Permutor;
+
+fn main() {
+    let max = 10;
+    let key: [u8; 32] = [0xBA; 32];
+    let permutor = Permutor::new_with_slice_key(max, key);
+    for permuted in permutor {
+        println!("{}", permuted);
+    }
+}
