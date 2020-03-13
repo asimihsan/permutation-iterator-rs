60a31e075aabb9a286450cd82aab56c67fe190cd
Asim Ihsan <asim.ihsan@gmail.com>
2020-03-12 20:23:26 -0700

    test/permutor - rename variables for clarity

--- tests/permutor.rs
+++ tests/permutor.rs
@@ -43,9 +43,10 @@ mod tests {
 
     #[test]
     fn test_small_battery_returns_correct_permutations() {
-        for i in 2..50 {
+        for max_value in 2..50 {
             for key in 0..10 {
-                get_permutation_values(i, key);
+                println!("testing max_value {} key {}", max_value, key);
+                get_permutation_values(max_value, key);
             }
         }
     }
@@ -55,27 +56,27 @@ mod tests {
         get_permutation_values(25_000, 0);
     }
 
-    fn get_permutation_values(max: u64, key: u64) -> Vec<u64> {
+    fn get_permutation_values(max_value: u64, key: u64) -> Vec<u64> {
         // === given ===
-        let permutor = Permutor::new_with_u64_key(max, key);
+        let permutor = Permutor::new_with_u64_key(max_value, key);
 
         // === when ===
         let result: Vec<u64> = permutor.collect();
 
         // === then ===
         assert_eq!(
-            max as usize,
+            max_value as usize,
             result.len(),
             "incorrect size result given max {} key {}",
-            max,
+            max_value,
             key
         );
         let result_unique: HashSet<u64> = HashSet::from_iter(result.clone());
         assert_eq!(
-            max as usize,
+            max_value as usize,
             result_unique.len(),
             "values aren't unique i.e. not a permutation given max {} key {}",
-            max,
+            max_value,
             key
         );
 
